20e6028baeb0ed63161fafb0d528a6d05d01937a
jeremy wambugu <wambugujeremy@gmail.com>
2020-07-22 04:47:48 +0300

    inital commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hambuger = document.getElementById('hamburger')
+const navUl = document.getElementById('nav-ul')
+
+hambuger.addEventListener('click', () => {
+    navUl.classList.toggle('show')
+})
